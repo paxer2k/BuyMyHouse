cd1dc780e9e9ad76dcdc08482ee03ac4b9b38df8
paxer2k <alex.arkhipov.7590@gmail.com>
2023-10-29 19:16:26 +0100

    final touches

--- Service/Commands/EmailCommandService.cs
+++ Service/Commands/EmailCommandService.cs
@@ -49,8 +49,8 @@ namespace Service.Commands
 
             if (mortgage.MortgageStatus == MortgageStatus.Declined)
             {
-                htmlContent = $"<div><strong>Dear {customer.FirstName} {customer.LastName},</strong><br>" +
-                    $"<p>After reviewing your mortgage application, we regret to inform you that it has been declined.</p>" +
+                htmlContent = $"<div>Dear {customer.FirstName} {customer.LastName},<br>" +
+                    $"<p>After reviewing your mortgage application, we regret to inform you that it has been <strong>declined</strong>.</p>" +
                     $"<p>If you would like to know the reason behind the decline, please use the contact information below:</p>" +
                     $"<p>Contact email: alex.arkhipov.testmail@gmail.com</p>" +
                     $"<p>Contact phone: 068432842</p>" +
@@ -60,8 +60,8 @@ namespace Service.Commands
 
             if (mortgage.MortgageStatus == MortgageStatus.Approved)
             {
-                htmlContent = $"<div><strong>Dear {customer.FirstName} {customer.LastName},</strong><br>" +
-                    $"<p>After reviewing your application, we are pleased to inform you that it has been <strong>approved<strong>.</p>" +
+                htmlContent = $"<div>Dear {customer.FirstName} {customer.LastName},<br>" +
+                    $"<p>After reviewing your application, we are pleased to inform you that it has been <strong>approved</strong>.</p>" +
                     $"<p>You can view your mortgage application through this <a href=\"{_appConfiguration.SendGridConfig.MortgageApplicationEndpoint}{mortgage.Id}\">link</a>.</p>" +
                     $"<p><strong>NOTE:</strong> The link to your mortgage application will be valid for the next 24 hours.</p>" +
                     $"<p>Kind regards,</p>" +
